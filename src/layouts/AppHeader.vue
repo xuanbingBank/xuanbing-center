f0ac906af8ae927eea30513e388efb32ed2459b8
玄冰 <1300272284@qq.com>
2024-12-01 17:27:10 +0800

    🔨 refactor: 增加@material-design-icons/svg:图标库

--- src/layouts/AppHeader.vue
+++ src/layouts/AppHeader.vue
@@ -1,10 +1,28 @@
 <template>
   <el-header class="app-header">
     <div class="header-logo">XuanBing Center</div>
+    <div class="header-actions">
+      <el-button class="action-btn">
+        <el-icon>
+          <Remove />
+        </el-icon>
+      </el-button>
+      <el-button class="action-btn">
+        <el-icon>
+          <FullScreen />
+        </el-icon>
+      </el-button>
+      <el-button class="action-btn close-btn">
+        <el-icon>
+          <Close />
+        </el-icon>
+      </el-button>
+    </div>
   </el-header>
 </template>
 
 <script lang="ts" setup>
+import { Remove, FullScreen, Close } from '@element-plus/icons-vue'
 </script>
 
 <style lang="less" scoped>
@@ -16,6 +34,7 @@
   border-bottom: @border-width-base @border-style-base @border-color-base;
   padding: 0 @spacing-lg;
   height: @header-height;
+  -webkit-app-region: drag; // 允许拖拽窗口
 }
 
 .header-logo {
@@ -28,5 +47,28 @@
   display: flex;
   align-items: center;
   gap: @spacing-sm;
+  -webkit-app-region: no-drag; // 按钮区域不允许拖拽
+}
+
+.action-btn {
+  padding: 4px;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  transition: background-color @animation-duration-base @animation-timing-function-base;
+
+  &:hover {
+    background-color: @menu-item-hover-bg;
+  }
+
+  .el-icon {
+    width: 16px;
+    height: 16px;
+  }
+
+  &.close-btn:hover {
+    background-color: #e81123;
+    color: white;
+  }
 }
 </style> 
